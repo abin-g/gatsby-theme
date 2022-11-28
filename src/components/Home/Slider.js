9ae2dd263afdc6d01abb9a7adb4524eb6752a921
Abin G <abings002@gmail.com>
2022-11-28 21:37:43 +0530

    Updates

--- src/components/Home/Slider.js
+++ src/components/Home/Slider.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Carousel } from 'antd';
-
+import { Carousel, Button, Tooltip } from 'antd';
+import { BsFillCursorFill, BsFillHandIndexThumbFill, BsFillTelephoneForwardFill, BsWhatsapp, BsUiChecksGrid } from "react-icons/bs";
 const contentStyle = {
     height: '500px',
     color: '#fff',
@@ -14,20 +14,49 @@ const contentStyle = {
 function Slider() {
 
     return (
-        <Carousel dotPosition="right">
-            <div>
-                <img style={contentStyle} src="https://jssors8.azureedge.net/demos/image-slider/img/px-beach-daylight-fun-1430675-image.jpg" />
+        <>
+            <Carousel dotPosition="right">
+                <div>
+                    <img style={contentStyle} src="https://jssors8.azureedge.net/demos/image-slider/img/px-beach-daylight-fun-1430675-image.jpg" />
+                </div>
+                <div>
+                    <img style={contentStyle} src="https://jssors8.azureedge.net/demos/image-slider/img/px-beach-daylight-fun-1430675-image.jpg" />
+                </div>
+                <div>
+                    <img style={contentStyle} src="https://jssors8.azureedge.net/demos/image-slider/img/px-beach-daylight-fun-1430675-image.jpg" />
+                </div>
+                <div>
+                    <img style={contentStyle} src="https://jssors8.azureedge.net/demos/image-slider/img/px-beach-daylight-fun-1430675-image.jpg" />
+                </div>
+            </Carousel>
+            <div className="icon-bar">
+                <div className="apply-cta">
+                    <Tooltip title="Apply Online">
+                        <Button shape="circle" style={{ background: '#E30613' }} icon={<BsFillCursorFill fill="white" size={20} />} />
+                    </Tooltip>
+                </div>
+                <div className="apply-cta">
+                    <Tooltip title="Enquir Now">
+                        <Button shape="circle" style={{ background: '#F47D07' }} icon={<BsFillHandIndexThumbFill fill="white" size={20} />} />
+                    </Tooltip>
+                </div>
+                <div className="apply-cta">
+                    <Tooltip title="Request a call">
+                        <Button shape="circle" style={{ background: '#FD4F00' }} icon={<BsFillTelephoneForwardFill fill="white" size={20} />} />
+                    </Tooltip>
+                </div>
+                <div className="apply-cta">
+                    <Tooltip title="Whatsapp">
+                        <Button shape="circle" style={{ background: '#19A56E' }} icon={<BsWhatsapp fill="white" size={20} />} />
+                    </Tooltip>
+                </div>
+                <div className="apply-cta">
+                    <Tooltip title="Events & News">
+                        <Button shape="circle" style={{ background: '#E50159' }} icon={<BsUiChecksGrid fill="white" size={20} />} />
+                    </Tooltip>
+                </div>
             </div>
-            <div>
-                <img style={contentStyle} src="https://jssors8.azureedge.net/demos/image-slider/img/px-beach-daylight-fun-1430675-image.jpg" />
-            </div>
-            <div>
-                <img style={contentStyle} src="https://jssors8.azureedge.net/demos/image-slider/img/px-beach-daylight-fun-1430675-image.jpg" />
-            </div>
-            <div>
-                <img style={contentStyle} src="https://jssors8.azureedge.net/demos/image-slider/img/px-beach-daylight-fun-1430675-image.jpg" />
-            </div>
-        </Carousel>
+        </>
     );
 };
 
